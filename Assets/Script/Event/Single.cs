0f484b68b572f202245370fb3d8f4f37d7a237c7
raven <674404416@qq.com>
2021-01-30 16:00:43 +0800

    添加单例模式模板

---
+++ Assets/Script/Event/Single.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single<T> where T : new()
+{
+    public static T I
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new T();
+            return m_instance;
+        }
+    }
+
+    private static T m_instance;
+}
